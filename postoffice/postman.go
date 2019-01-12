8962962edf1b7bf65aa3d3317417a998d8489879
weiguo.ni <neefrankie@gmail.com>
2019-01-12 22:30:08 +0800

    postoffice pkg

--- postoffice/postman.go
+++ postoffice/postman.go
@@ -1,1 +1,33 @@
 package postoffice
+
+import (
+	"github.com/go-mail/mail"
+)
+
+// Postman wraps mail dialer.
+type Postman struct {
+	dialer *mail.Dialer
+}
+
+// NewPostman creates a new instance of PostOffice
+func NewPostman(host string, port int, user, pass string) Postman {
+	return Postman{
+		dialer: mail.NewDialer(host, port, user, pass),
+	}
+}
+
+// Deliver asks the postman to deliver a parcel.
+func (pm Postman) Deliver(p Parcel) error {
+	m := mail.NewMessage()
+
+	m.SetAddressHeader("From", p.FromAddress, p.FromName)
+	m.SetAddressHeader("To", p.ToAddress, p.ToName)
+	m.SetHeader("Subject", p.Subject)
+	m.SetBody("text/plain", p.Body)
+
+	if err := pm.dialer.DialAndSend(m); err != nil {
+		return err
+	}
+
+	return nil
+}
