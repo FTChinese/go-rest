a224c85f3bff6415670012d250956af412ceaa09
weiguo.ni <neefrankie@gmail.com>
2019-03-03 23:16:02 +0800

    add ToInt method

--- param.go
+++ param.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 // Param represents a pair of query parameter from URL.
@@ -18,7 +19,7 @@ func GetQueryParam(req *http.Request, key string) Param {
 
 	return Param{
 		key:   key,
-		value: v,
+		value: strings.TrimSpace(v),
 	}
 }
 
@@ -35,4 +36,19 @@ func (p Param) ToString() (string, error) {
 	}
 
 	return p.value, nil
+}
+
+// ToInt converts the value of a query parameter to int64
+func (p Param) ToInt() (int64, error) {
+	if p.value == "" {
+		return 0, fmt.Errorf("%s have empty value", p.key)
+	}
+
+	num, err := strconv.ParseInt(p.value, 10, 0)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return num, nil
 }
