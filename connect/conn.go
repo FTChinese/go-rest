5ed1af56822f0f9e8ac3717b28eaf70a07e7fbb9
weiguo.ni <neefrankie@gmail.com>
2021-11-06 10:54:20 +0800

    refactor: removed conn and postman package

--- connect/conn.go
+++
@@ -1,9 +0,0 @@
-package connect
-
-// Connect represents a connection to a server or database.
-type Connect struct {
-	Host string `mapstructure:"host"`
-	Port int    `mapstructure:"port"`
-	User string `mapstructure:"user"`
-	Pass string `mapstructure:"pass"`
-}
